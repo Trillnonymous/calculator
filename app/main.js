809c3d5712e653cb0502208c4288c7783be96ed1
Trillnonymous <trillnonymous@gmail.com>
2018-04-26 19:18:25 -0400

    Update main.js

--- app/main.js
+++ app/main.js
@@ -1,10 +1,17 @@
-(function(window){
+(function(window, $){
 	var Calculator = {
 		version: '0.0.1',
 		value: '',
+		loaded: false,
 		operators: [],
 		fns: {}
 	};
 	
+	Calculator.load = function(callback){
+		this.loaded = true;
+		if (callback !== undefined && typeof callback == 'function')
+			callback.apply(this, []);
+	};
+	
 	window.Calculator = Calculator;
-}(this === window ? this : window));
+}(this === window ? this : window, jQuery));
