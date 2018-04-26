c4758bedec00b02bbecd75dc52cc134a1cd8203a
Trillnonymous <trillnonymous@gmail.com>
2018-04-26 19:14:16 -0400

    Create main.js

---
+++ app/main.js
@@ -0,0 +1,10 @@
+(function(window){
+	var Calculator = {
+		version: '0.0.1',
+		value: '',
+		operators: [],
+		fns: {}
+	};
+	
+	window.Calculator = Calculator;
+}(this === window ? this : window));
